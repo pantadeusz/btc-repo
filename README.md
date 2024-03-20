9f1b9538b483fe152d07b7609e6f76c948e44df9
Tadeusz <niepodam@tajne.com>
2024-03-20 15:24:19 +0100

    przyklad we flask

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # btc-repo
+
+```bash
+flask --app app run --port 8080 --host 0.0.0.0
+```
--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 from flask import Flask
+import json
+import subprocess
 
 app = Flask(__name__)
 
+def get_balance():
+    return json.loads(subprocess.check_output(["bitcoin-cli", "-conf=/home/testnet/bitcoin.conf", "getbalances"]))
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    balance = get_balance()
+    confirmed = 0
+    unconfirmed = 0
+    confirmed = balance["mine"]["trusted"]
+    unconfirmed = balance["mine"]["untrusted_pending"]
+    return "<h2>Zbieram na czesne tb1qgj5x6kw7a6dsx87fl5ysrfrnzxz3uj0pcp9ekm</h2><p>" + str(confirmed) + " " + str(unconfirmed) + "</p>"
